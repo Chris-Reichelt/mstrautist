5f1a56885d0c01dae4d65dd96ed308e1d357bc2d
Chris Reichelt <reicheltcm@gmail.com>
2024-10-27 14:48:29 +0000

    v0.66

--- app.py
+++ app.py
@@ -15,11 +15,10 @@ def get_mstr_data():
     mstr_btc=get_mstr_btc()
     mrkt_cap=mstr.fast_info['marketCap']
     hist = mstr.history(period='5y')['Close']
-    data = mstr.history(period='1d')['Close'].iloc[-1]
-    if not data.empty:
-      current_price = data['Close'].iloc[-1]
+    try:
+      current_price = mstr.history(period='1d')['Close'].iloc[-1]
     else:
-      current_price = None  # Or set a default value, such as 0
+      current_price=hist.iloc[-1]
     shares=mstr.info['impliedSharesOutstanding'] 
     insiders=mstr.insider_roster_holders
     employees=mstr.info['fullTimeEmployees']

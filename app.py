506f51f611a448fc4aeec726405e85069051225a
Chris Reichelt <reicheltcm@gmail.com>
2024-11-11 15:10:32 +0000

    v0.70

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ def get_mstr_data():
     try:
       current_price = mstr.history(period='1d')['Close'].iloc[-1]
     except: 
-      current_price=hist.iloc[-2]
+      current_price=0
     try:  
       shares=mstr.info['impliedSharesOutstanding'] 
     except:
